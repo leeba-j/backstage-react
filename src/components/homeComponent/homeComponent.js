f83cf7d0fa32d2bb22a42c9850a6b158ce46fb41
leeba-julius <julius.leeba@absa.africa>
2022-06-28 14:52:42 +0200

    Add components

---
+++ src/components/homeComponent/homeComponent.js
@@ -0,0 +1,11 @@
+import homeComponent from "./homeComponent.css";
+
+const Home = () => {
+  return (
+    <div className="HomeApp">
+      <h1>Home Component</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,40 @@
+import "./navBar.css";
+const changeColor = (event) => {
+  removeAllClassNames();
+  document
+    .querySelector(`.${event.target.className}`)
+    .classList.add("selected");
+};
+
+const removeAllClassNames = () =>
+  document
+    .querySelectorAll(".navbar-content ul li")
+    .forEach((item) => item.classList.remove("selected"));
+
+const NavBar = () => {
+  return (
+    <div className="NavBarApp">
+      <nav>
+        <h3>NavBar</h3>
+        <div className="navbar-content">
+          <ul>
+            <li className="item-1" onClick={(event) => changeColor(event)}>
+              Item 1
+            </li>
+            <li className="item-2" onClick={(event) => changeColor(event)}>
+              Item 2
+            </li>
+            <li className="item-3" onClick={(event) => changeColor(event)}>
+              Item 3
+            </li>
+            <li className="item-4" onClick={(event) => changeColor(event)}>
+              Item 4
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
